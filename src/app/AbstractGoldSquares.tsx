8ee19926d9f92d0bac35a84fd4ccac52b2c42c52
Guilherme Silva Fernandes <2003silvagui@gmail.com>
2025-07-25 21:14:58 -0300

    feat: more

--- src/app/AbstractGoldSquares.tsx
+++ src/app/AbstractGoldSquares.tsx
@@ -54,7 +54,7 @@ export default function AbstractGoldSquares() {
                 -webkit-text-fill-color: transparent;
                 background-clip: text;
                 text-fill-color: transparent;
-                font-size: 50px;
+                font-size: 45px;
                 width: 270px;
                 text-align: center;
               }
@@ -64,16 +64,59 @@ export default function AbstractGoldSquares() {
                 -webkit-text-fill-color: transparent;
                 background-clip: text;
                 text-fill-color: transparent;
-                font-size: 16px;
-                width: 270px;
+                font-size: 14px;
+                text-align: center;
+              }
+              .gold-gradient-month {
+                background: linear-gradient(90deg, #FFD700 0%, #FFF8DC 50%, #FFD700 100%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                background-clip: text;
+                text-fill-color: transparent;
+                font-size: 18px;
+                text-align: center;
+              }
+              .gold-gradient-number {
+                background: linear-gradient(90deg, #FFD700 0%, #FFF8DC 50%, #FFD700 100%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                background-clip: text;
+                text-fill-color: transparent;
+                font-size: 50px;
                 text-align: center;
               }
             `}</style>
-            <h1 className="absolute top-36 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center font-alex-brush text-4xl pointer-events-none select-none gold-gradient-text">
-              Alícia
-            </h1>
-            <span className="font-alex-brush absolute top-50 left-1/2 -translate-x-1/2 -translate-y-1/2 gold-gradient-old-text">15 anos</span>
-            <span className="font-alata absolute top-[270px] left-1/2 -translate-x-1/2 -translate-y-1/2 gold-gradient-subtitle">Você está convidado para celebrar esse dia especial comigo!</span>
-        </div>
-    );
-} 
+      <h1 className="absolute top-36 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center font-alex-brush text-4xl pointer-events-none select-none gold-gradient-text">
+        Alícia
+      </h1>
+      <span className="font-alex-brush absolute top-48 left-1/2 -translate-x-1/2 -translate-y-1/2 gold-gradient-old-text">15 anos</span>
+      <span className="font-alata absolute top-[260px] left-1/2 -translate-x-1/2 -translate-y-1/2 gold-gradient-subtitle text-sm">Você está convidado para celebrar esse dia especial comigo!</span>
+      <DataComponent />
+    </div>
+  );
+}
+
+
+const DataComponent = () => {
+  return (
+    <div className="fixed top-[370px] left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+      <div className="flex flex-col items-center">
+        <div className="w-20 h-px bg-gradient-to-r from-transparent via-yellow-400 to-yellow-400 mb-1"></div>
+        <span className="font-alata gold-gradient-subtitle text-center">Sábado</span>
+        <div className="w-20 h-px bg-gradient-to-l from-transparent via-yellow-400 to-yellow-400 mt-1"></div>
+      </div>
+      
+      <div className="flex flex-col items-center">
+        <span className="font-alata gold-gradient-month">SETEMBRO</span>
+        <span className="font-alata gold-gradient-number font-bold leading-none">06</span>
+        <span className="font-alata gold-gradient-month text-lg mt-2">2025</span>
+      </div>
+      
+      <div className="flex flex-col items-center">
+        <div className="w-20 h-px bg-gradient-to-r from-transparent via-yellow-400 to-yellow-400 mb-1"></div>
+        <span className="font-alata gold-gradient-subtitle text-center">Às 19:30</span>
+        <div className="w-20 h-px bg-gradient-to-l from-transparent via-yellow-400 to-yellow-400 mt-1"></div>
+      </div>
+    </div>
+  )
+}
